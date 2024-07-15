edc8b973b8861cf913eba995870b435fb84d7448
Khoi Nguyen <nbkhoi@tma.com.vn>
2024-07-15 11:07:53 +0700

    Init CDK

---
+++ test/infra-tenant-user-pool.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfraTenantUserPool from '../lib/infra-tenant-user-pool-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infra-tenant-user-pool-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfraTenantUserPool.InfraTenantUserPoolStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
